f380779e80df52f24ee99aff879550fd05416c99
Vijay <capvijju24@gmail.com>
2025-01-08 12:29:24 +0530

    resolved build error

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Github, Twitter, Linkedin, Mail } from "lucide-react";
+import { Github, Linkedin, Mail } from "lucide-react";
 import Link from "next/link";
 
 export const Footer = () => {
