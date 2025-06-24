4e99898c748d874e4155269f75535bc8d7f33a67
Vijay <capvijju24@gmail.com>
2025-06-24 13:10:32 +0530

    removed revalidate for problem page

--- app/problem/[id]/page.tsx
+++ app/problem/[id]/page.tsx
@@ -3,8 +3,8 @@ import getProblem from "@/actions/getProblem";
 import ProblemPage from "@/components/pages/problemPage";
 import { notFound } from "next/navigation";
 
-//revalidate every 3600 seconds
-export const revalidate = 3600;
+// revalidate every 3600 seconds
+// export const revalidate = 24 * 3600;
 
 //dynamic params true -- "[id]"
 export const dynamicParams = true;
