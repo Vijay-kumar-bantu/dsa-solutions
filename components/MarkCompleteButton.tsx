5f94829dcf2c17819ed8356c70312032b2739e1d
Vijay <capvijju24@gmail.com>
2025-09-13 17:42:11 +0530

    added mark as complete button

---
+++ components/MarkCompleteButton.tsx
@@ -0,0 +1,36 @@
+import { useCompletion } from "@/contexts/CompletionContext";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import { Loader } from "./Loader";
+
+function MarkCompleteButton({ problemId }: { problemId: string }) {
+	const [checkLoading, setCheckLoading] = useState<boolean>(false);
+	const { isCompleted, toggleCompletion } = useCompletion();
+	const completed = isCompleted(problemId);
+
+	if (checkLoading) {
+		return <Loader size="medium" />;
+	}
+
+	const handleAction = async () => {
+		setCheckLoading(true);
+		await toggleCompletion(problemId);
+		setCheckLoading(false);
+	};
+	return (
+		<motion.button
+			onClick={handleAction}
+			whileHover={{ scale: 1.02 }}
+			whileTap={{ scale: 0.98 }}
+			className={`inline-flex items-center space-x-2 px-4 py-2 ${
+				completed
+					? "bg-red-600 hover:bg-red-700"
+					: "bg-green-600 hover:bg-green-700"
+			} text-white rounded-lg transition-colors`}
+		>
+			<span>{completed ? "Mark as Incomplete" : "Mark as Complete"}</span>
+		</motion.button>
+	);
+}
+
+export default MarkCompleteButton;
